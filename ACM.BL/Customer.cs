f2d80df990def322c7b222dfe1dfa4ce4783c53e
Zeheva <Zeheva@yahoo.com>
2017-06-09 19:36:20 -0400

    First test project set up

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,20 @@
+﻿namespace ACM.BL
+{
+    public class Customer
+    {
+        private string _lastName;
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public string FirstName { get; set; }
+        public string MyProperty { get; set; }
+        public string EmailAddress { get; set; }
+        public int CustomerID { get; private set; }
+
+        public string FullName { get { return LastName + ", " + FullName; } }
+
+    }
+}
---
+++ ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //Arrange
+            Customer customer = new Customer();
+            customer.FirstName = "Bilbo";
+            customer.LastName = "Baggins";
+
+            string expected = "Baggins, Bilbo";
+            //Act
+
+            string actual = customer.FullName;
+
+            //Assert
+
+            Assert.AreEqual(expected, actual);
+
+
+
+        }
+    }
+}
